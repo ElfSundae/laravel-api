7cf7679d3bb56b5bde9f74dc4a15e913a4b8fd0d
Elf Sundae <elf.sundae@gmail.com>
2017-06-12 06:45:32 +0800

    Add ExampleTest

---
+++ tests/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ElfSundae\Laravel\Api\Test;
+
+class ExampleTest extends \PHPUnit\Framework\TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
