2e15dc6594bbffdd56f34b1fe54319b7d0683210
Elf Sundae <elf.sundae@gmail.com>
2017-06-13 02:44:52 +0800

    Add current_app_key() helper

--- src/helpers.php
+++ src/helpers.php
@@ -13,3 +13,17 @@ if (! function_exists('api')) {
         return new ApiResponse(...$args);
     }
 }
+
+if (! function_exists('current_app_key')) {
+    /**
+     * Get the app key of the current api client.
+     *
+     * @see \ElfSundae\Laravel\Api\Http\Middleware\VerifyApiToken
+     *
+     * @return string|null
+     */
+    function current_app_key()
+    {
+        return app('request')->attributes->get('current_app_key');
+    }
+}
