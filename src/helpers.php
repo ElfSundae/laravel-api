a9b1920ad7a31e6c6fda4515f8367ffbdf20bb5e
Elf Sundae <elf.sundae@gmail.com>
2017-06-13 02:33:22 +0800

    Add helper api()

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,15 @@
 <?php
+
+use ElfSundae\Laravel\Api\Http\ApiResponse;
+
+if (! function_exists('api')) {
+    /**
+     * Create a new API response.
+     *
+     * @return \ElfSundae\Laravel\Api\Http\ApiResponse
+     */
+    function api(...$args)
+    {
+        return new ApiResponse(...$args);
+    }
+}
