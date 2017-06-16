2497f65d6ac5c341898f2d71baf6c01be5714154
Elf Sundae <elf.sundae@gmail.com>
2017-06-17 05:36:59 +0800

    Add ProfileJsonResponse middleware

---
+++ src/Http/Middleware/ProfileJsonResponse.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ElfSundae\Laravel\Api\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\JsonResponse;
+
+class ProfileJsonResponse
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @see https://github.com/barryvdh/laravel-debugbar/issues/252#issuecomment-248343762
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        if (
+            $response instanceof JsonResponse &&
+            app()->bound('debugbar') &&
+            app('debugbar')->isEnabled() &&
+            is_object($response->getData())
+        ) {
+            $response->setData($response->getData(true) + [
+                '_debugbar' => app('debugbar')->getData(),
+            ]);
+        }
+
+        return $response;
+    }
+}
