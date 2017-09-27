95f9272a2e1d215f79f3009b70299f90e276b186
Elf Sundae <elf.sundae@gmail.com>
2017-09-27 14:17:47 +0800

    Add `Helper::addAcceptableJsonTypeForRequest()`

--- src/Helper.php
+++ src/Helper.php
@@ -2,7 +2,9 @@
 
 namespace ElfSundae\Laravel\Api;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Container\Container;
 
 class Helper
 {
@@ -34,5 +36,36 @@ class Helper
     public static function getCurrentAppKey(Request $request)
     {
         return $request->attributes->get(static::CURRENT_APP_KEY);
+    }
+
+    /**
+     * Add JSON type to the "Accept" header for the request.
+     *
+     * @see https://laravel-china.org/topics/3430
+     *
+     * @param  \Illuminate\Container\Container  $app
+     * @param  callable  $determination  ($request) :bool
+     * @param  callable  $callback  ($request)
+     * @return mixed
+     */
+    public static function addAcceptableJsonTypeForRequest(Container $app, $determination = null, $callback = null)
+    {
+        $app->rebinding('request', function ($app, $request) use ($determination, $callback) {
+            if (is_null($determination) || call_user_func($determination, $request)) {
+                $accept = $request->headers->get('Accept');
+
+                if (! Str::contains($accept, ['/json', '+json'])) {
+                    $accept = rtrim('application/json,'.$accept, ',');
+
+                    $request->headers->set('Accept', $accept);
+                    $request->server->set('HTTP_ACCEPT', $accept);
+                    $_SERVER['HTTP_ACCEPT'] = $accept;
+
+                    if ($callback) {
+                        call_user_func($callback, $request);
+                    }
+                }
+            }
+        });
     }
 }
